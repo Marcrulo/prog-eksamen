ccb4e28317d59de3207e7d9059de5ab88b3292c8
Marcrulo <marcus.presutti.eu@gmail.com>
2020-03-11 11:51:37 +0100

    g

--- scrape_police.py
+++ scrape_police.py
@@ -24,10 +24,38 @@ for i in range(days-1):
 		day1 = i
 		day2 = i+1
 	
-	
+	 
 
 	URL = BASE_URL + "-{0}{1}-{2}-{3}/{3}/{4}/{1}".format(day1,day2,month,year,month_index)
 	print(URL)
 
 	
-day2 = i+1
+	# Get HTML as text
+	page = requests.get(URL)
+
+	# Create soup object, which makes it possible to filter the HTML 
+	soup = BeautifulSoup(page.content, "html.parser")
+
+	# Filter/find div by class name. Returns entire tags
+	content = soup.find("div", {"class": "rich-text"})
+
+	# Strips the text from the tags
+	print(content.text.strip())
+
+
+	# Find the second p-tag with the certain class. This returns a date 
+	Dato = soup.findAll("p", {"class": "newsInfoText body-xmedium-police-regular dark-blue"})[1]
+	txt = Dato.text.strip()
+	print(txt)
+
+
+	''' Put content in txt-file '''
+	# Write to txt-file
+	file = open("tmpfile{}.txt".format(str(day1)+str(day2)), "w", encoding="UTF-8")
+
+	# Seperate content section by newline
+	lines = content.text.strip().split('\n')
+	for line in lines:
+	    if not line_empty(line):
+	        file.write(line+'\n')
+	file.close()
