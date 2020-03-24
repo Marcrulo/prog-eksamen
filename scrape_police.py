a856d0ace2b6426e2a1ecaf291ddffbd592a2443
Marcus Presutti <marcus@presutti.eu>
2020-03-24 12:18:32 +0100

    Regex fixed

--- scrape_police.py
+++ scrape_police.py
@@ -1,4 +1,5 @@
 import requests # Make URL requests
+import re
 from bs4 import BeautifulSoup # Filter through fetched HTML-code
 
 
@@ -6,56 +7,90 @@ def line_empty(line):
     return len(line.strip()) < 1
 
 
-BASE_URL = "https://politi.dk/nordsjaellands-politi/doegnrapporter/uddrag-af-doegnrapport"
+'''
+https://politi.dk/doegnrapporter
+?fromDate=2020/2/1
+&toDate=2020/2/29
+&newsType=D%C3%B8gnrapporter
+&page=1
+&district=Nordsjaellands-Politi
+'''
+
+fromDate = "2020/2/1" # Year/Month/Day
+toDate = "2020/2/29"  # Year/Month/Day
+URL = "https://politi.dk/doegnrapporter?fromDate={}&toDate={}&newsType=D%C3%B8gnrapporter&page={}&district=Nordsjaellands-Politi".format(fromDate,toDate,1)
+
+#print(URL)
+
+frontPage = requests.get(URL)
+soup = BeautifulSoup(frontPage.content, "html.parser")
+
+ngCtrl = soup.find('section', {'ng-controller': 'newsListController'})
+#print(ngCtrl)
+
+urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str(ngCtrl))
+for url in urls:
+	print(url)
+'''
+file = open("FRONTPAGE.html",'w',encoding="UTF-8")
+file.write(str(ngCtrl))
+
+links = soup.find_all("a", {"class": "newsResultLink"})
+'''
 
 
-month = "februar"
-month_index = '02' # Inkluder 0 (fx 02, 03, 10, 11)
-days = 29
-year = 2020
-
-for i in range(days-1):
-	
-	if i < 10:
-		day1 = "0"+str(i)
-		if i+1 < 10:
-			day2 = "0"+str(i+1)
-	else:
-		day1 = i
-		day2 = i+1
-	
-	 
-
-	URL = BASE_URL + "-{0}{1}-{2}-{3}/{3}/{4}/{1}".format(day1,day2,month,year,month_index)
-	print(URL)
-
-	
-	# Get HTML as text
-	page = requests.get(URL)
-
-	# Create soup object, which makes it possible to filter the HTML 
-	soup = BeautifulSoup(page.content, "html.parser")
-
-	# Filter/find div by class name. Returns entire tags
-	content = soup.find("div", {"class": "rich-text"})
-
-	# Strips the text from the tags
-	print(content.text.strip())
 
 
-	# Find the second p-tag with the certain class. This returns a date 
-	Dato = soup.findAll("p", {"class": "newsInfoText body-xmedium-police-regular dark-blue"})[1]
-	txt = Dato.text.strip()
-	print(txt)
+
+'''
+linksAvailable = True
+page = 1
+while linksAvailable:
+	pass
+'''
 
 
-	''' Put content in txt-file '''
-	# Write to txt-file
-	file = open("tmpfile{}.txt".format(str(day1)+str(day2)), "w", encoding="UTF-8")
 
-	# Seperate content section by newline
-	lines = content.text.strip().split('\n')
-	for line in lines:
-	    if not line_empty(line):
-	        file.write(line+'\n')
-	file.close()
+
+
+
+
+
+
+
+'''
+# Get HTML as text
+page = requests.get(URL)
+
+# Create soup object, which makes it possible to filter the HTML 
+soup = BeautifulSoup(page.content, "html.parser")
+
+print(soup.title.text)
+if soup.title.text == "Runtime":
+	print("Runtime Error!!!\n") 
+
+
+# Filter/find div by class name. Returns entire tags
+content = soup.find("div", {"class": "rich-text"})
+
+# Strips the text from the tags
+#print(content.text.strip())
+
+
+# Find the second p-tag with the certain class. This returns a date 
+Dato = soup.findAll("p", {"class": "newsInfoText body-xmedium-police-regular dark-blue"})[1]
+txt = Dato.text.strip()
+#print(txt)
+
+
+# Put content in txt-file
+# Write to txt-file
+file = open("tmpfile{}.txt".format(str(day1)+str(day2)), "w", encoding="UTF-8")
+
+# Seperate content section by newline
+lines = content.text.strip().split('\n')
+for line in lines:
+    if not line_empty(line):
+        file.write(line+'\n')
+file.close()
+'''
