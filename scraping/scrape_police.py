d18c801f7c49a79f6d34954f0cea8070dd02b69b
Marcus Presutti <marcus@presutti.eu>
2020-03-25 11:01:12 +0100

    Title problem fixed

--- scraping/scrape_police.py
+++ scraping/scrape_police.py
@@ -6,8 +6,8 @@ def line_empty(line):
     return len(line.strip()) < 1
 
 # Search criteria
-fromDate = "2020/2/1" # Year/Month/Day
-toDate = "2020/2/29"  # Year/Month/Day
+fromDate = "2020/1/15" # Year/Month/Day
+toDate = "2020/2/15"  # Year/Month/Day
 page = 1 			  # Page ... pages are done manually due to risks of request limit
 
 URL = "https://politi.dk/doegnrapporter?fromDate={}&toDate={}&newsType=D%C3%B8gnrapporter&page={}&district=Nordsjaellands-Politi".format(fromDate,toDate,page)
@@ -24,22 +24,18 @@ if len(urls) == 0:
 for url in urls:	
 	subPage = requests.get(url)
 	bs = BeautifulSoup(subPage.content, "html.parser")
-	title = bs.find("h1",{"class":"h1-police-bold dark-blue"}).text.strip()
+
 	content = bs.find("div", {"class": "rich-text"})
+	date = bs.find_all('p',{'class':'newsInfoText body-xmedium-police-regular dark-blue'})[1].text.strip()
 
-	title_regex = re.findall(r'[0-9]+', title)
-	for i in range(len(title_regex)):
-		title_regex[i] = title_regex[i].lstrip('0')
-
-
-	file = open("../page_content/{}-{}-{}.txt".format(title_regex[0],title_regex[1],title_regex[2]), "w", encoding="UTF-8")
+	file = open("../page_content/{}.txt".format(date), "w", encoding="UTF-8")
 
 	lines = content.text.strip().split('\n') 
 	for line in lines:
 	    file.write(line+'\n')
 
 	file.close()
-
+	
 
 # Extract relevant data from content
 
