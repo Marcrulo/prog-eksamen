d18dc943ea88a12dca5479d04681d493177cb27b
TheShortDuck <andreas.a.eriksen@gmail.com>
2020-03-25 11:26:25 +0100

    word_count looper alle filer i page_content

--- data_analysis/word_count.py
+++ data_analysis/word_count.py
@@ -1,24 +1,35 @@
 import json
-
+import glob
 
 countsDict = {}
-def word_count(str):
-    words = str.split()
 
-    for word in words:  #Looper igennem ord i den splittede string
-        if word in countsDict:
-            countsDict[word] += 1  #Hvis ordet eksisterer sæt antal 1 op 
-        else:
-            countsDict[word] = 1  #Ellers tilføj ord med værdien 1
+def word_count():
+	data = ""
+	files = glob.glob("../page_content/*.txt")
 
-    countsJson = json.dumps(countsDict)  #Konverter dict til json
-    return countsDict
+	for i in range(len(files)):
+		with open(files[i], encoding='utf8') as file:
+			temp = file.read().replace("\n"," ")
+			data += temp
+		file.close()
 
-with open("../page_content/16-17-2020.txt") as file:
-	data = file.read().replace("\n","")
+	words = data.split()
 
-word_count(data)
+	for word in words:  #Looper igennem ord i den splittede string
+		if word in countsDict:
+			countsDict[word] += 1  #Hvis ordet eksisterer sæt antal 1 op 
+		else:
+			countsDict[word] = 1  #Ellers tilføj ord med værdien 1
+
+	#Konverter dict til json
+	with open ("word_count.json","w") as out:
+		out.write(json.dumps(countsDict))
+	return countsDict
+
+word_count()
+
 sorted_dict=sorted(countsDict.items(), key=lambda x: x[1])
-#print(sorted_dict)
 for item in sorted_dict:
 	print(item)
+
+
