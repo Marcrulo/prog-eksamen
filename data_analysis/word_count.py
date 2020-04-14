d8280d46c6ba5dca8418853f45134a6eabe5181a
TheShortDuck <andreas.a.eriksen@gmail.com>
2020-04-14 12:26:01 +0200

    ord på 1 medtages uden at fjerne symboler

--- data_analysis/word_count.py
+++ data_analysis/word_count.py
@@ -16,15 +16,15 @@ def word_count():
 	words = data.lower().split()
 	
 	for word in words:  #Looper igennem ord i den splittede string
-		symbols = ['.',',',';',':','!','?'] #hvis der dukkker gentagelser af ord op i json, skal flere symboler evt tilføjes
-		if(len(word) > 2):
+		symbols = ['.',',',';',':','!','?']  #hvis der dukkker gentagelser af ord op i json, skal flere symboler evt tilføjes
+		if(len(word)-1):  #Fjerner symboler efter ord der er længere end 1 og ikke er tal
 			if word[-1] in symbols and word[-2].isnumeric() is False:
 				word = word[:-1]
 
-			if word in countsDict:
-				countsDict[word] += 1  #Hvis ordet eksisterer sæt antal 1 op 
-			else:
-				countsDict[word] = 1  #Ellers tilføj ord med værdien 1
+		if word in countsDict:
+			countsDict[word] += 1  #Hvis ordet eksisterer sæt antal 1 op 
+		else:
+			countsDict[word] = 1  #Ellers tilføj ord med værdien 1
 
 	#Konverter dict til json
 	with open ("word_count.json","w", encoding='utf8') as out:
