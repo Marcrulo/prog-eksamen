05cb70a0c66392066a5630fa54c652907a5c6242
Marcus Presutti <marcus@presutti.eu>
2020-04-14 12:15:54 +0200

    update word count

--- data_analysis/word_count.py
+++ data_analysis/word_count.py
@@ -17,13 +17,14 @@ def word_count():
 	
 	for word in words:  #Looper igennem ord i den splittede string
 		symbols = ['.',',',';',':','!','?'] #hvis der dukkker gentagelser af ord op i json, skal flere symboler evt tilføjes
-		if len(word) > 2 and word[-1] in symbols and word[-2].isnumeric() is False:
-			word = word[:-1]
+		if(len(word) > 2):
+			if word[-1] in symbols and word[-2].isnumeric() is False:
+				word = word[:-1]
 
-		if word in countsDict:
-			countsDict[word] += 1  #Hvis ordet eksisterer sæt antal 1 op 
-		else:
-			countsDict[word] = 1  #Ellers tilføj ord med værdien 1
+			if word in countsDict:
+				countsDict[word] += 1  #Hvis ordet eksisterer sæt antal 1 op 
+			else:
+				countsDict[word] = 1  #Ellers tilføj ord med værdien 1
 
 	#Konverter dict til json
 	with open ("word_count.json","w", encoding='utf8') as out:
