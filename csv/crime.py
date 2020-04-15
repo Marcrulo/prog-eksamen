acad215064ff39b7a2a9e81fc095921662a6d78f
Marcus Presutti <marcus@presutti.eu>
2020-04-15 19:05:49 +0200

    crime.py update

--- csv/crime.py
+++ csv/crime.py
@@ -1,41 +1,81 @@
-import requests
 import csv
 import glob
 
 
 # Write to CSV file
 csvfile = open('crime.csv', "w", encoding='utf-8',newline="")
+writer = csv.writer(csvfile, delimiter=';', quotechar="'", quoting=csv.QUOTE_ALL)
 files = glob.glob("../page_content/*.txt")
-
-points = {
-	'driving' : 0,
-	'drugs'   : 0,
-	'lethal'  : 0,
-	'other'   : 0,
-	'stealing': 0,
-	'violence': 0,
-	'index'   : 0,
-}
 
 for file in files:
 	with open(file, encoding='utf8') as txtfile:
 		
 		a = txtfile.read().split('\n\n')
-		for item in a:
-			print(item)
-			print("----")
+		for i in range(len(a)):
+			
+		
+			points = {
+				'driving' : 0,
+				'drugs'   : 0,
+				'lethal'  : 0,
+				'other'   : 0,
+				'stealing': 0,
+				'violence': 0,
+				'index'   : i,
+			}
+			section = a[i].lower().split()
+		
+			for sec in section:
+				symbols = ['.',',',';',':','!','?']
+				if(len(sec)-1):  
+					if sec[-1] in symbols and sec[-2].isnumeric() is False:
+						sec = sec[:-1]
+			
+			
+			for word in section:  
+				with open('../keywordlists/driving.txt', encoding='utf-8') as driving:					
+					for keyword in driving.read().split('\n'):
+						if word.startswith(keyword):
+							points['driving'] += 1
+				
+				with open('../keywordlists/drugs.txt', encoding='utf-8') as drugs:
+					for keyword in drugs.read().split('\n'):
+						if word.startswith(keyword):
+							points['drugs'] += 1
 
-		# Loop gennem afsnit 
-		#    reset point
-		#    For hvert afsnit:
-		#       loop gennem keywordlists
-		#          tilføj point
-		#          writer = csv.writer(csvfile, delimiter=';', quotechar="'", quoting=csv.QUOTE_ALL)
+				with open('../keywordlists/lethal.txt', encoding='utf-8') as lethal:
+					for keyword in lethal.read().split('\n'):
+						if word.startswith(keyword):
+							points['lethal'] += 1
+
+				with open('../keywordlists/other.txt', encoding='utf-8') as other:
+					for keyword in other.read().split('\n'):
+						if word.startswith(keyword):
+							points['other'] += 1
+
+				with open('../keywordlists/stealing.txt', encoding='utf-8') as stealing:
+					for keyword in stealing.read().split('\n'):
+						if word.startswith(keyword):
+							points['stealing'] += 1
+
+				with open('../keywordlists/violence.txt', encoding='utf-8') as violence:
+					for keyword in violence.read().split('\n'):
+						if word.startswith(keyword):
+							points['violence'] += 1
+				
+				
+
+			id = file.replace('../page_content\\','') + '#' + str(points['index'])
+			writer.writerow([id,points['driving'],points['drugs'],points['lethal'],points['other'],points['stealing'],points['violence']])			
+			
+			#print(points['driving'],points['drugs'],points['lethal'],points['other'],points['stealing'],points['violence'])
+
+		
+		
 
 
 
 	txtfile.close()
-
 
 
 
