59bdc92a2c577ee8aaa413219e1198c25b1f0de5
Marcus Presutti <marcus@presutti.eu>
2020-04-16 20:49:58 +0200

    procfile

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fpgdtkivkpmedt:cd7f60ed50a88ef2bb2caf1a7e24f00ce37959decf2dddbb90ab1654df9d3816@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/dlfqf6ig6k8vb'
 
 db = SQLAlchemy(app)
 
@@ -22,12 +22,14 @@ def webhook():
     db.session.commit()
     return "user created"
 
+'''
 @app.route('/delete/')
 def delete():
     u = User.query.get(i)
     db.session.delete(u)
     db.session.commit()
     return "user deleted"
+'''
 
 if __name__ == '__main__':
     app.run()
