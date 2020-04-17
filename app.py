6b6629c2e0ea0800ffc516ea97d319791a0eb25c
Marcus Presutti <marcus@presutti.eu>
2020-04-17 13:40:35 +0200

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+class Crimes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=False, nullable=False)
@@ -15,10 +15,27 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+class Cities(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
 
+    def __repr__(self):
+        return '<User %r>' % self.username      
+
+class MLresult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+#db.drop_all()
 #db.create_all()
-db.session.add(User(username="Fast", email="examples@example.com"))
-db.session.commit()
+#db.session.add(User(username="Fast", email="examples@example.com"))
+#db.session.commit()
+
 
 @app.route('/')
 def hello_world():
