19625bd80bb078eaca7c7ad368063e71707b2d87
Marcus Presutti <marcus@presutti.eu>
2020-04-17 13:29:36 +0200

    database

--- app.py
+++ app.py
@@ -2,34 +2,27 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fpgdtkivkpmedt:cd7f60ed50a88ef2bb2caf1a7e24f00ce37959decf2dddbb90ab1654df9d3816@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/dlfqf6ig6k8vb'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import User
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+#db.create_all()
+db.session.add(User(username="Fast", email="examples@example.com"))
+db.session.commit()
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
-@app.route('/add/')
-def webhook():
-    name = "ram"
-    email = "ram@ram.com"
-    u = User(id = id, nickname = name, email = email)
-    print("user created", u)
-    db.session.add(u)
-    db.session.commit()
-    return "user created"
-
-'''
-@app.route('/delete/')
-def delete():
-    u = User.query.get(i)
-    db.session.delete(u)
-    db.session.commit()
-    return "user deleted"
-'''
 
 if __name__ == '__main__':
     app.run()
