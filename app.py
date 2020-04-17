913c5830be25355726e6c7c9bc4cb34eed065a10
Marcus Presutti <marcus@presutti.eu>
2020-04-17 18:15:15 +0200

    update

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
-app.config['DATABASE_URL'] = "postgres://togcikqgjukane:38eb8b72b77222f835c9854ad7074a96736cc6f26aa68b8360405549e3aeb0f6@ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432/de6j0v0fhdad84"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+#app.config['DATABASE_URL'] = "postgres://togcikqgjukane:38eb8b72b77222f835c9854ad7074a96736cc6f26aa68b8360405549e3aeb0f6@ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432/de6j0v0fhdad84"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
