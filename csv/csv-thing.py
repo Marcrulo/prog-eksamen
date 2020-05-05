b600d82380918e4decfc2e9f7e09c6c556668954
Marcus Presutti <marcus@presutti.eu>
2020-05-05 19:47:48 +0200

    json

--- csv/csv-thing.py
+++ csv/csv-thing.py
@@ -12,23 +12,3 @@ writer = csv.writer(csvfile, delimiter=';', quotechar="'", quoting=csv.QUOTE_ALL
 ''' Data auditing i csv fil (program til at checke om data opfylder det rigtige format) '''
 
 
-
-''' USELESS, JUST IGNORE
-row = [';', ';', ';', ';', ';']
-pattern = "[0-9][0-9].[0-9][0-9]"
-pattern_alder = "[0-9][0-9]-årig"
-
-
-while True:
-    line = file.readline()
-    row[0] = line.strip()
-    line = file.readline()
-    row[1] = line.strip()
-    row[2] = line.strip().split(':')[0]
-
-    # Her skal vi udtrække data og lægge i row-listen
-    writer.writerow(row)
-    if not line:
-        break
-file.close()
-'''
