aff63c003976eb4f686ff9cb83d810a5ad7a589e
Marcrulo <marcus.presutti.eu@gmail.com>
2020-05-05 12:41:49 +0200

    update

--- cluster/citySpecific.py
+++ cluster/citySpecific.py
@@ -6,7 +6,8 @@ import numpy as np
 months = ['januar', 'februar', 'marts','april','maj','juni','juli','august','september','oktober','november','december']
 categories = ["date","driving","drugs","lethal","other","stealing","violence","severity","city"]
 
-monthAverage = 30.4368499
+monthAverage = 100000000000#30.4368499
+cityName = 'allerød'
 
 df = read_csv("../csv/crime.csv", sep=";", names=categories)
 for item in categories[1:-1]:
@@ -19,10 +20,20 @@ for i in range(len(df['date'])):
 
 #print(df['date'])
 
-df.loc()
+#df = df[df['city']=='hillerød']
+df = DataFrame(df.loc[df['city'].str.contains(cityName,case=False)])
+print(df)
 
 kmeans = KMeans(n_clusters=12).fit(df[["date","severity"]])
 centroids = kmeans.cluster_centers_
+
+averageList = []
+for item in centroids:
+	averageList.append(item[1])
+average = sum(averageList)/12
+
+print(average)
+
 
 ticks = []
 for i in range(1,13):
@@ -30,9 +41,11 @@ for i in range(1,13):
 labels = ['Jan', 'Feb', 'Mar', 'Apr','Maj','Jun','Jul','Aug','Sep','Okt','Nov','Dec']
 
 plt.figure()
+plt.axhline(average,0,10000000,color='purple')
 plt.xticks(ticks, labels,rotation=20)
 #plt.show()
 
+plt.title(label=cityName.capitalize())
 plt.scatter(df['date'], df['severity'], s=50, alpha=0.5, c= kmeans.labels_.astype(float))
 plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
 plt.xlabel("Date")
